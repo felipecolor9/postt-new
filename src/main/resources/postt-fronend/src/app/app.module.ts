b9a9a8a057eeae38d42576ff96513e0ba7dcb58d
Felipe Marques Silva <felipe.m199@gmail.com>
2021-07-23 19:38:56 -0300

    Core module imported; Default routes defined

--- src/main/resources/postt-fronend/src/app/app.module.ts
+++ src/main/resources/postt-fronend/src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { RouterModule } from '@angular/router';
+import { Error404Component } from './error-404/error-404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Error404Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    RouterModule.forRoot([
+      {
+        path:'', redirectTo:'home', pathMatch: 'full'
+      },
+      {
+        path:'**', component: Error404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
