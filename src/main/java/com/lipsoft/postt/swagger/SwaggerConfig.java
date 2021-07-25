fc2026e213cefab57eebd630b5665085c660c13d
Felipe Marques Silva <felipe.m199@gmail.com>
2021-07-25 19:23:33 -0300

    Swagger config file added

---
+++ src/main/java/com/lipsoft/postt/swagger/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.lipsoft.postt.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    @Bean
+    public ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("ACCESS Control")
+                .description(" Lipsoft control-access API code")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .contact(new Contact("Felipe Marques", "https://github.com/felipecolor9", "felipe.m199@gmail.com"))
+                .build();
+    }
+}
