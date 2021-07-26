6ca96cb1cf9a708d4bdd77b3c114497b01d8c4d9
felipecolor9 <felipe.m199@gmail.com>
2021-07-26 20:40:11 -0300

    Form group/builder added

--- webapp/postt-fronend/src/app/postit/register-postit/register-postit.component.ts
+++ webapp/postt-fronend/src/app/postit/register-postit/register-postit.component.ts
@@ -1,22 +1,35 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, } from "@angular/forms";
 import { ActivatedRoute } from "@angular/router";
 import { Postit } from "../postit.model";
 import { PostitService } from "../postit.service";
 
 @Component({
     selector: 'app-register-postit',
-    templateUrl: './register-postit.component.html'
+    templateUrl: './register-postit.component.html',
 })
 export class RegisterPostitComponent implements OnInit {
 
-    constructor(private activatedRoute: ActivatedRoute, private postitService: PostitService) { }
+    postitForm: FormGroup;
 
-    postit:Postit
+    constructor(private formBuilder: FormBuilder, private activatedRoute: ActivatedRoute, private postitService: PostitService) { }
  
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.createForm(new Postit());
+    }
+
+    createForm(postit:Postit) {
+        this.postitForm = this.formBuilder.group({
+            title: '',
+            details: ''
+        })
+        this.postitForm.valueChanges.subscribe(console.log); 
+    }
 
     //TODO: finish save function
     save(): void {
-        this.postitService.save(this.postit).subscribe();
+        
     }
+  
+
 }
