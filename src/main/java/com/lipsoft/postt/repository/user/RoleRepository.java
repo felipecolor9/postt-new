d33e082da6371b7b24fcdee81ab4e43ba4e1c62b
felipecolor9 <felipe.m199@gmail.com>
2021-11-01 12:12:11 -0300

    Role repository created.

---
+++ src/main/java/com/lipsoft/postt/repository/user/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.lipsoft.postt.repository.user;
+
+import com.lipsoft.postt.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
