6d4bd6e5b0f84239e4a038d4b2cba1f6cd4a76a0
felipecolor9 <felipe.m199@gmail.com>
2021-11-01 12:11:56 -0300

    Custom JPA method added (findByUsername).

--- src/main/java/com/lipsoft/postt/repository/user/UserRepository.java
+++ src/main/java/com/lipsoft/postt/repository/user/UserRepository.java
@@ -1,4 +1,4 @@
-package com.lipsoft.postt.repository;
+package com.lipsoft.postt.repository.user;
 
 import com.lipsoft.postt.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+    User findByUsername(String username);
 }
