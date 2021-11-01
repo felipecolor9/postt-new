32f480dc40304560d9ad088add8a4d2d21270d91
felipecolor9 <felipe.m199@gmail.com>
2021-11-01 12:13:45 -0300

    Mapping annotation added to ignore certain properties.

--- src/main/java/com/lipsoft/postt/mapper/UserMapper.java
+++ src/main/java/com/lipsoft/postt/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package com.lipsoft.postt.mapper;
 import com.lipsoft.postt.dto.request.UserDTO;
 import com.lipsoft.postt.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
@@ -10,6 +11,10 @@ public interface UserMapper {
 
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
+    @Mapping(target = "creationDate", ignore = true)
+    @Mapping(target = "lastAccessDate", ignore = true)
+    @Mapping(target = "postitList", ignore = true)
+    @Mapping(target = "roles", ignore = true)
     User toModel(UserDTO userDTO);
 
     UserDTO toDTO(User user);
