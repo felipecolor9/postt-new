c52d6a5531075ac5a2c9741d0dbf526b077bb21b
felipecolor9 <felipe.m199@gmail.com>
2021-11-03 13:21:19 -0300

    Unused methods removed.

--- src/main/java/com/lipsoft/postt/controller/UserController.java
+++ src/main/java/com/lipsoft/postt/controller/UserController.java
@@ -1,55 +1,49 @@
 package com.lipsoft.postt.controller;
 
 import com.lipsoft.postt.dto.request.UserDTO;
+import com.lipsoft.postt.exception.InvalidCredentialsException;
 import com.lipsoft.postt.exception.UsernameAlreadyInUseException;
-import com.lipsoft.postt.model.Role;
 import com.lipsoft.postt.model.User;
 import com.lipsoft.postt.service.UserService;
-import com.lipsoft.postt.service.security.RoleService;
-import lombok.Data;
+import com.lipsoft.postt.service.security.SecurityService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.net.URI;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
+@Slf4j
 public class UserController {
 
     @Autowired
     UserService userService;
     @Autowired
-    RoleService roleService;
+    SecurityService securityService;
 
     @GetMapping("/users")
     public ResponseEntity<List<UserDTO>> getUsers() {
         return ResponseEntity.ok(userService.getUsers());
     }
 
-    @PostMapping("/user/save")
-    public ResponseEntity<User> saveUser(@RequestBody UserDTO userDTO) throws UsernameAlreadyInUseException {
+    @PostMapping("/user/register")
+    public ResponseEntity<User> register(@RequestBody UserDTO userDTO) throws UsernameAlreadyInUseException {
         var uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
-        return ResponseEntity.created(uri).body(userService.saveUser(userDTO)); }
-
-    @PostMapping("/role/save")
-    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
-        var uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
-        return ResponseEntity.created(uri).body(roleService.saveRole(role)); }
-
-    @PostMapping("/role/addtouser")
-    public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form) {
-        roleService.addRoleToUser(form.getUsername(), form.getRoleName());
-        return ResponseEntity.ok().build();
+        return ResponseEntity.created(uri).body(userService.register(userDTO));
     }
-}
 
-@Data
-class RoleToUserForm {
-    private String username;
-    private String roleName;
+    //Not working properly
+    @GetMapping("/token/refresh")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        securityService.refreshToken(request,response);
+    }
 }
