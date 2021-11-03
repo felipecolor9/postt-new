a60ebd5243b2ca642109365ae0e00928b59dd068
felipecolor9 <felipe.m199@gmail.com>
2021-11-03 13:25:04 -0300

    User service simplified.

--- src/main/java/com/lipsoft/postt/service/UserService.java
+++ src/main/java/com/lipsoft/postt/service/UserService.java
@@ -2,11 +2,10 @@ package com.lipsoft.postt.service;
 
 import com.lipsoft.postt.dto.request.UserDTO;
 import com.lipsoft.postt.dto.response.MessageResponseDTO;
-import com.lipsoft.postt.exception.InvalidCredentialsException;
 import com.lipsoft.postt.exception.UsernameAlreadyInUseException;
 import com.lipsoft.postt.mapper.UserMapper;
 import com.lipsoft.postt.model.User;
-import com.lipsoft.postt.repository.PostitRepository;
+import com.lipsoft.postt.repository.user.RoleRepository;
 import com.lipsoft.postt.repository.user.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,42 +21,42 @@ import java.util.stream.Collectors;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final PostitRepository postitRepository;
+    private final RoleRepository roleRepository;
     private final MessageResponseDTO messageResponseDTO;
     private final UserMapper userMapper = UserMapper.INSTANCE;
     private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    UserService(UserRepository userRepository, PostitRepository postitRepository, MessageResponseDTO messageResponseDTO, PasswordEncoder passwordEncoder) {
+    UserService(UserRepository userRepository, RoleRepository roleRepository, MessageResponseDTO messageResponseDTO, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
-        this.postitRepository = postitRepository;
+        this.roleRepository = roleRepository;
         this.messageResponseDTO = messageResponseDTO;
         this.passwordEncoder = passwordEncoder;
     }
 
-    //Login section
-    public boolean login(UserDTO userDTO) throws InvalidCredentialsException {
-        var userToLogin = userMapper.toModel(userDTO);
+    public User register(UserDTO userDTO) throws UsernameAlreadyInUseException {
+        var userToSave = userMapper.toModel(userDTO);
+        userToSave.setRoles(new ArrayList<>());
 
-        if (isCredentialsValid(userToLogin.getUsername(), userDTO.getPassword())) {
-            userToLogin.setLastAccessDate(LocalDateTime.now());
-            userRepository.save(userToLogin);
-            return true;
-        } else throw new InvalidCredentialsException();
+        if (isUsernameAvailable(userToSave.getUsername())) {
+            userToSave.setPassword(passwordEncoder.encode(userToSave.getPassword()));
+            userToSave.getRoles().add(roleRepository.findByName("ROLE_USER"));
+            userToSave.setCreationDate(LocalDateTime.now());
+            log.info("Saving new user {} to the database", userToSave.getUsername());
+            return userRepository.save(userToSave);
+
+        } else throw new UsernameAlreadyInUseException(userToSave.getUsername());
     }
 
+    //validation methods
     private boolean isUsernameAvailable(String username) {
         return userRepository.findAll().stream()
                 .noneMatch(user -> user.getUsername().equals(username));
     }
 
-    private boolean isCredentialsValid(String username, String password) {
-        return userRepository.findAll().stream().anyMatch(user -> user.getUsername().equals(username) && user.getPassword().equals(password));
-    }
-
     //Common API functions
     public UserDTO getUser(String username) {
-        log.info("Fetching user {} to user {}", username);
+        log.info("Fetching user {}", username);
         return userMapper.toDTO(userRepository.findByUsername(username));
     }
 
@@ -66,18 +66,4 @@ public class UserService {
                 .map(userMapper::toDTO)
                 .collect(Collectors.toList());
     }
-
-    public User saveUser(UserDTO userDTO) throws UsernameAlreadyInUseException {
-        var userToSave = userMapper.toModel(userDTO);
-        log.info("Saving new user {} to the database", userToSave.getUsername());
-        //Encode password
-        userToSave.setPassword(passwordEncoder.encode(userToSave.getPassword()));
-
-        if (isUsernameAvailable(userToSave.getUsername())) {
-            userToSave.setCreationDate(LocalDateTime.now());
-            return userRepository.save(userToSave);
-
-        } else throw new UsernameAlreadyInUseException(userToSave.getUsername());
-    }
-
 }
