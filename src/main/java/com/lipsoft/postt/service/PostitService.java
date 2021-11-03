9d10280c0815158144c15061a0a87587b325ecaa
felipecolor9 <felipe.m199@gmail.com>
2021-11-03 16:57:30 -0300

    Method that fetch all postits according to the username stored in session (SecurityContextHolder).

--- src/main/java/com/lipsoft/postt/service/PostitService.java
+++ src/main/java/com/lipsoft/postt/service/PostitService.java
@@ -5,7 +5,10 @@ import com.lipsoft.postt.dto.response.MessageResponseDTO;
 import com.lipsoft.postt.exception.PostitNotFoundException;
 import com.lipsoft.postt.mapper.PostitMapper;
 import com.lipsoft.postt.repository.PostitRepository;
+import com.lipsoft.postt.repository.user.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -13,21 +16,25 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class PostitService {
 
     private final PostitRepository postitRepository;
+    private final UserRepository userRepository;
     private final MessageResponseDTO messageResponseDTO;
     private final PostitMapper postitMapper = PostitMapper.INSTANCE;
 
     @Autowired
-    PostitService (PostitRepository postitRepository, MessageResponseDTO messageResponseDTO) {
+    PostitService (PostitRepository postitRepository, UserRepository userRepository, MessageResponseDTO messageResponseDTO) {
         this.postitRepository = postitRepository;
+        this.userRepository = userRepository;
         this.messageResponseDTO = messageResponseDTO;
     }
 
     public MessageResponseDTO add(PostitDTO postitDTO) {
         var postitToSave = postitMapper.toModel(postitDTO);
         postitToSave.setCreationDate(LocalDateTime.now());
+        postitToSave.setUser(userRepository.findByUsername(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString()));
         postitRepository.save(postitToSave);
         return MessageResponseDTO
                 .builder()
@@ -35,16 +42,13 @@ public class PostitService {
                 .build();
     }
 
-    public void addAll(List<PostitDTO> postitsDTO) {
-        postitsDTO.stream()
-                .map(postitMapper::toModel)
-                .forEach(postitRepository::save);
-    }
-
-    public List<PostitDTO> findAll() {
-        return postitRepository.findAll().stream()
+    public List<PostitDTO> findAllByUsername(String username) {
+        var postits= postitRepository.findAll().stream()
+                .filter(postit -> postit.getUser().getUsername().equals(username))
                 .map(postitMapper::toDTO)
                 .collect(Collectors.toList());
+        log.info("Fetching all postits from {}", username);
+        return postits;
     }
 
     public PostitDTO findByID(Long id) throws PostitNotFoundException {
