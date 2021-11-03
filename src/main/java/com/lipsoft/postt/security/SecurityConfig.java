1ff7ad6fc14a7d5ea0b7e6873ac56bbc6a15c89e
felipecolor9 <felipe.m199@gmail.com>
2021-11-03 13:24:14 -0300

    Tests configs.

--- src/main/java/com/lipsoft/postt/security/SecurityConfig.java
+++ src/main/java/com/lipsoft/postt/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.lipsoft.postt.security;
 
 import com.lipsoft.postt.filter.CustomAuthenticationFilter;
+import com.lipsoft.postt.filter.CustomAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,9 +12,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.GET;
-import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
@@ -31,18 +32,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    //Change authorization to postits
     protected void configure(HttpSecurity http) throws Exception {
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
         customAuthenticationFilter.setFilterProcessesUrl("/api/login");
 
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().antMatchers("/api/login/**").permitAll();
-        http.authorizeRequests().antMatchers(GET, "/api/users").hasAnyAuthority("ROLE_USER");
-        http.authorizeRequests().antMatchers(POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
+        http.authorizeRequests().antMatchers("/h2/**").permitAll(); //Dev environment
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll(); //Dev environment
+        http.authorizeRequests().antMatchers("/api/user/login/**", "/api/token/refresh/**", "/api/user/register/**").permitAll();
+        http.authorizeRequests().antMatchers(GET, "/api/users").hasAnyAuthority("ROLE_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
 
+        http.csrf().disable(); //Dev
+        http.headers().frameOptions().disable(); //Dev
+
         http.addFilter(customAuthenticationFilter);
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
